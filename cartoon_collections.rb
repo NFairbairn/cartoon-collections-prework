6ad67f57d3d9fe6c466f5f01ddf96e6d8614ae3e
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-30 19:33:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls (planeteer_calls)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (cheddar_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_cheese & cheese_types
 end
